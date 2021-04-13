14cdd50eb9adf2d1b4db5e46bcdfa3eaed2bd28f
D.Three <ducdd@fabbi.io>
2021-04-13 09:00:05 +0700

    bai 1, sua, them m la so phan tu cua mang duoc tach ra

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var array1 = [],
     array3 = [],
     newArray = []
 var n = array.length;
-console.log(n);
+console.log(n, "kjkjkj");
 var m = n / 3;
 array1 = array.slice(0, m);
 array2 = array.slice(m, (2 * m));
@@ -12,7 +12,7 @@ array3 = array.slice((2 * m), (3 * m));
 console.log(array1);
 console.log(array2);
 console.log(array3);
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i < m; i++) {
     newArray.push(array1[i], array2[i], array3[i]);
 }
 console.log(newArray);
