388863bbf39da88d6f9c4030d4e0d256509fc1a6
D.Three <ducdd@fabbi.io>
2021-04-13 08:58:23 +0700

    bai 1, them tham so m la so phan tu cua mang con

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ var array1 = [],
     array2 = [],
     array3 = [],
     newArray = []
-array1 = array.slice(0, 3);
-array2 = array.slice(3, 6);
-array3 = array.slice(6, 9);
+var n = array.length;
+console.log(n);
+var m = n / 3;
+array1 = array.slice(0, m);
+array2 = array.slice(m, (2 * m));
+array3 = array.slice((2 * m), (3 * m));
 console.log(array1);
 console.log(array2);
 console.log(array3);
@@ -13,3 +16,5 @@ for (var i = 0; i < 3; i++) {
     newArray.push(array1[i], array2[i], array3[i]);
 }
 console.log(newArray);
+
+// bai 2
