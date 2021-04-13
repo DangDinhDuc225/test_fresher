1567d70a6ee7ca9b51d034390ed74d93dd63302a
D.Three <ducdd@fabbi.io>
2021-04-13 11:33:30 +0700

    bai 3

--- index.js
+++ index.js
@@ -28,38 +28,60 @@
 // console.log(newArray);
 
 // bai 2
-var array = [0, 100, -4, 100, 100, 5, 100, 100]
-var soLon1, soLon2;
-soLon1 = array[0];
-soLon2 = array[0];
-var sum;
-var n = 0;
-for (var i = 0; i < array.length; i++) {
-    if (soLon1 < array[i + 1]) {
-        soLon1 = array[i + 1];
-    }
-}
-console.log(soLon1)
-for (var i = 0; i < array.length; i++) {
-    if (soLon1 === array[i]) {
-        n = n + 1;
-    }
-}
-console.log(n);
+// var array = [0, 100, -4, 100, 100, 5, 100, 100]
+// var soLon1, soLon2;
+// soLon1 = array[0];
+// soLon2 = array[0];
+// var sum;
+// var n = 0;
+// for (var i = 0; i < array.length; i++) {
+//     if (soLon1 < array[i + 1]) {
+//         soLon1 = array[i + 1];
+//     }
+// }
+// console.log(soLon1)
+// for (var i = 0; i < array.length; i++) {
+//     if (soLon1 === array[i]) {
+//         n = n + 1;
+//     }
+// }
+// console.log(n);
 
-if (n > 1) {
-    soLon2 = soLon1;
-} else {
-    for (var i = 0; i < array.length; i++) {
-        if (soLon2 < array[i + 1]) {
-            if (array[i + 1] !== soLon1) {
-                soLon2 = array[i + 1];
-            }
+// if (n > 1) {
+//     soLon2 = soLon1;
+// } else {
+//     for (var i = 0; i < array.length; i++) {
+//         if (soLon2 < array[i + 1]) {
+//             if (array[i + 1] !== soLon1) {
+//                 soLon2 = array[i + 1];
+//             }
+//         }
+//     }
+// }
+// console.log(soLon2);
+// sum = soLon1 + soLon2;
+// console.log(sum);
+
+// bai 3
+var array = [1, 5, 4, 7, 9, 0, -10, 13, 93, 14, 15];
+var array2 = [];
+var n = 0;
+var hieuNhoNhat = array[1] - array[0];
+for (var i = 0; i < (array.length - 1); i++) {
+    for (var j = i + 1; j < array.length; j++) {
+        hieu = Math.abs(array[i] - array[j]);
+        if (hieuNhoNhat > hieu) {
+            hieuNhoNhat = hieu;
         }
     }
 }
-console.log(soLon2);
-sum = soLon1 + soLon2;
-console.log(sum);
-
-// bai 3
+console.log(hieuNhoNhat);
+for (var i = 0; i < (array.length - 1); i++) {
+    for (var j = i + 1; j < array.length; j++) {
+        hieu = Math.abs(array[i] - array[j]);
+        if (hieu === hieuNhoNhat) {
+            array2.push([array[i], array[j]]);
+        }
+    }
+}
+console.log(array2);
